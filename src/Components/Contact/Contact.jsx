3d66d7829fb9d2a273bc065c164ed5a59beb38f2
meshram-harsh <hmeshram112@gmail.com>
2024-07-13 01:08:38 +0530

    first commit

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
+
+const Contact = () => {
+    const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "b558d3cb-2957-44da-84b4-34ab3711098c");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+
+
+  return (
+    <div className='contact'>
+        <div className="contact-col">
+            <h3>Send us a message<img src={msg_icon} alt="" /></h3>
+            <p>Feel free to reach out through contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community.</p>
+            <ul>
+                <li> <img src={mail_icon} alt="" />meshram.harsh05@gmail.com</li>
+                <li><img src={phone_icon} alt="" />+91 7499288828</li>
+                <li><img src={location_icon} alt="" />House no. 1 - Indra Nagar, dighori, Nagpur <br /> 440036, India</li>
+            </ul>
+        </div>
+        <div className="contact-col">
+            <form onSubmit={onSubmit}>
+                <label>Your name</label>
+                <input type="text" name='name' placeholder='Enter your name' required/>
+
+                <label>Phone Number</label>
+                <input type="tel" name='phone' placeholder='Enter your mobile number' required/>
+
+                <label>Write your messege here</label>
+                <textarea name="messege" rows='6' placeholder='Enter your messege' required></textarea>
+                <button type='submit' className='btn dark-btn'>Submit now <img src={white_arrow} alt="" />
+                </button>
+            </form>
+            <span>{result}</span>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Contact
